15453906a1118df7a2473724c4c0c238b3c83755
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2024-01-23 22:58:25 +0600

    Header fix

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,15 +7,21 @@ import Logo from "../../public/munshi-enterprise.png";
 
 export default function Header() {
   const navLinks = [
-    { name: "Home", href: "/" },
+    { name: "Home", href: "/", dropDownMenu: false },
     {
       name: "About Us",
       href: "/about",
-      dropDrown: ["Our History", "Our Team", "Our Goal", "Membership"],
+      dropDownMenu: true,
+      dropDrown: [
+        { name: "Our History", href: "/" },
+        { name: "Our Team", href: "/" },
+        { name: "Our Goal", href: "/" },
+        { name: "Membership", href: "/" },
+      ],
     },
-    { name: "Our Products", href: "/products" },
-    { name: "Gallery", href: "/gallery" },
-    { name: "Contact Us", href: "/contact" },
+    { name: "Our Products", href: "/products", dropDownMenu: false },
+    { name: "Gallery", href: "/gallery", dropDownMenu: false },
+    { name: "Contact Us", href: "/contact", dropDownMenu: false },
   ];
   const pathname = usePathname();
 
@@ -30,50 +36,26 @@ export default function Header() {
             height={250}
           />
         </Link>
-        <ul className="group flex">
-          {navLinks.map((link, index) => {
-            const isActive: boolean =
-              link.href === "/"
-                ? pathname === "/"
-                : pathname.startsWith(link.href);
-
-            let dropdownContent = null;
-
-            if (link.dropDrown) {
-              dropdownContent = (
-                <ul className="absolute left-0 top-9 z-10 mt-2 hidden w-56 origin-top-right divide-y-2 divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 hover:block focus:outline-none group-hover:block">
-                  <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 hover:text-green-950">
-                    {link.dropDrown[0]}
-                  </li>
-                  <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 hover:text-green-950">
-                    {link.dropDrown[1]}
-                  </li>
-                  <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 hover:text-green-950">
-                    {link.dropDrown[2]}
-                  </li>
-                  <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 hover:text-green-950">
-                    {link.dropDrown[3]}
-                  </li>
-                </ul>
-              );
-            }
-            return (
-              <li
-                key={index}
-                className={
-                  isActive
-                    ? "relative cursor-pointer border-b-4 border-green-900 p-2 text-base font-bold uppercase text-green-900  hover:text-green-900"
-                    : "relative cursor-pointer p-2 text-base font-bold uppercase text-gray-700 hover:border-green-900 hover:text-green-900"
-                }
-              >
-                <Link href={link.href}>{link.name}</Link>
-                {dropdownContent}
-              </li>
-            );
-          })}
+        <ul className="flex">
+          {navLinks.map((link, index) => (
+            <li
+              key={index}
+              className={`relative cursor-pointer px-2 text-base font-bold ${
+                link.href === "/"
+                  ? pathname === "/"
+                    ? "border-b-2 border-green-900 text-green-900"
+                    : "text-gray-700 hover:border-green-900 hover:text-green-900"
+                  : pathname.startsWith(link.href)
+                    ? "border-b-2 border-green-900 text-green-900"
+                    : "text-gray-700 hover:border-green-900 hover:text-green-900"
+              }`}
+            >
+              <Link href={link.href}>{link.name}</Link>
+            </li>
+          ))}
         </ul>
         <div>
-          <button className="rounded-lg bg-green-900 p-2 font-bold uppercase text-white">
+          <button className="rounded-lg bg-green-900 p-2 font-bold text-white">
             Download Profile
           </button>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,12 @@ export default function Home() {
           <div className="relative">
             <Image
               src={Hero}
-              alt="Munshi Enterprise Logo"
-              width={1920}
-              height={728}
-              className="h-full w-full object-cover"
+              alt="Lorem Enterprise Logo"
+              className="h-full w-full object-cover object-center"
             />
             <div className="absolute inset-0 flex flex-col items-center justify-center gap-4">
               <article className="text-7xl text-green-950">
-                Welcome to Munshi Enterprise
+                Welcome to Lorem Enterprise
               </article>
               <p className="text-3xl text-white">
                 A <span className="italic">beau</span> approach to
@@ -28,17 +26,17 @@ export default function Home() {
         <div className="container mx-auto flex flex-col items-center py-8">
           <h1 className="mb-8 text-3xl">How it works</h1>
           <p className="text-2xl italic">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
-            libero, atque autem consectetur reprehenderit quia quo tenetur. Ea
-            minima possimus quasi alias error, ex molestias quod cupiditate
-            libero vitae beatae minus obcaecati neque! Amet maiores hic omnis
-            tenetur accusantium quaerat nulla porro veniam quae? Quisquam
-            laboriosam labore doloremque vero, aperiam pariatur voluptates,
-            placeat ab sunt mollitia iusto. Debitis est excepturi maiores nulla,
-            dolorem aut velit odit nam repellendus ab aliquid porro maxime neque
-            ipsum optio quis rerum soluta dicta corporis, amet voluptatem non
-            dolore quaerat. Quos velit delectus voluptates cum praesentium
-            quaerat ullam quas dolorum fuga, aliquam dignissimos natus optio.
+            In the serene twilight, a solitary figure traversed the ancient
+            forest, where whispered secrets lingered in the rustling leaves.
+            Moonlight spilled through the dense canopy, illuminating a path
+            paved with moss-covered stones. The air was infused with the
+            intoxicating fragrance of blooming wildflowers, as the night
+            symphony echoed with the symphony of nocturnal creatures. Lost in
+            contemplation, the travelers thoughts meandered through the realms
+            of forgotten tales and uncharted dreams. Shadows danced on the
+            fringes of reality, casting a surreal ambiance upon the journey.
+            Each step resonated with the heartbeat of the forest, creating an
+            ethereal connection between the wanderer and the mystical landscape.
           </p>
         </div>
       </main>
